0d4b08dc3e7ef18830600f5ff72adb5f3eb0c9d3
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-09 06:20:42 +0530

    chore: add opensea actions and readme

--- packages/plugins/opensea/README.md
+++ packages/plugins/opensea/README.md
@@ -1,31 +1,27 @@
-# Superfluid Plugin
+# Opensea Plugin
 
-Superfluid plugin allows you to interact with Superfluid contracts.
+Opensea plugin allows you to interact with Opensea API.
 
 ## Installation
 ```bash
-npm install @anton-stack/superfluid-plugin
+npm install @anton-stack/opensea-plugin
 ```
 
 ## Usage for AgentKit
 
 ```ts
 import { AgentKit } from '@coinbase/agentkit';
-import { superFluidActionProvider } from '@anton-stack/superfluid-plugin/agentkit';
+import { openseaActionProvider } from '@anton-stack/opensea-plugin/agentkit';
 
-const superfluid = superFluidActionProvider();
+const opensea = openseaActionProvider();
 
 const agentKit = await AgentKit.from({
     walletProvider, // Your Wallet Provider. eg- viem, cdp, etc.
-    actionProviders: [superfluid],
+    actionProviders: [opensea],
 });
 ```
 
 ## Tools
 
-- **Create or Update or Delete Flow**: Create, update, or delete a flow of tokens from sender to receiver.
-- **Get Flow Rate**: Get the current flowrate between a sender and receiver for a specific token.
-- **Update Member Units**: Update the units for a member in a Superfluid Pool.
-- **Get Member Units**: Get the units of a member in a Superfluid Pool.
-- **Get Member Flow Rate**: Get the flow rate of a member in a Superfluid Pool.
-- **Get Total Flow Rate**: Get the total flow rate of a Superfluid Pool.
+- **Get NFT Collection Statistics**: Get the statistics of an NFT collection.
+- **Get NFT Sales**: Get the recent sales of an NFT collection.
--- packages/plugins/opensea/src/agentkit/index.ts
+++ packages/plugins/opensea/src/agentkit/index.ts
@@ -4,30 +4,90 @@ import {
   type EvmWalletProvider,
   type Network,
 } from '@coinbase/agentkit';
-import { z } from 'zod';
+import type { z } from 'zod';
 
-const tempSchema = z.string();
-
-class TempActionProvider extends ActionProvider<EvmWalletProvider> {
-  constructor() {
-    super('temp', []);
+import {
+  GetNftCollectionStatisticsParametersSchema,
+  type GetNftCollectionStatisticsResponseSchema,
+  GetNftSalesParametersSchema,
+  type GetNftSalesResponseSchema,
+} from '../common/schema';
+class OpenseaActionProvider extends ActionProvider<EvmWalletProvider> {
+  private readonly apiKey: string;
+  constructor({ apiKey }: { apiKey: string }) {
+    super('opensea', []);
+    this.apiKey = apiKey;
   }
 
   @CreateAction({
-    name: 'temp.action',
-    description: 'placeholder',
-    schema: tempSchema,
+    name: 'opensea_get_collection_statistics',
+    description: 'Get NFT collection statistics',
+    schema: GetNftCollectionStatisticsParametersSchema,
   })
-  getQuote(
-    _walletProvider: EvmWalletProvider,
-    _parameters: z.infer<typeof tempSchema>
+  async getNftCollectionStatistics(
+    parameters: z.infer<typeof GetNftCollectionStatisticsParametersSchema>
   ) {
-    return 'placeholder';
+    let nftCollectionStatistics: z.infer<
+      typeof GetNftCollectionStatisticsResponseSchema
+    >;
+    try {
+      const response = await fetch(
+        `https://api.opensea.io/api/v2/collections/${parameters.collectionSlug}/stats`,
+        {
+          headers: {
+            accept: 'application/json',
+            'x-api-key': this.apiKey,
+          },
+        }
+      );
+
+      nftCollectionStatistics = (await response.json()) as z.infer<
+        typeof GetNftCollectionStatisticsResponseSchema
+      >;
+    } catch (error) {
+      throw new Error(`Failed to get NFT collection statistics: ${error}`);
+    }
+
+    return nftCollectionStatistics;
   }
 
-  supportsNetwork(network: Network): boolean {
-    return network.protocolFamily === 'evm';
+  @CreateAction({
+    name: 'opensea_get_sales',
+    description: 'Get recent NFT Sales',
+    schema: GetNftSalesParametersSchema,
+  })
+  async getNftSales(parameters: z.infer<typeof GetNftSalesParametersSchema>) {
+    let nftSales: z.infer<typeof GetNftSalesResponseSchema>;
+    try {
+      const response = await fetch(
+        `https://api.opensea.io/api/v2/events/collection/${parameters.collectionSlug}?event_type=sale&limit=5`,
+        {
+          headers: {
+            accept: 'application/json',
+            'x-api-key': this.apiKey,
+          },
+        }
+      );
+
+      nftSales = (await response.json()) as z.infer<
+        typeof GetNftSalesResponseSchema
+      >;
+    } catch (error) {
+      throw new Error(`Failed to get NFT sales: ${error}`);
+    }
+
+    return nftSales.asset_events.map((event) => {
+      return {
+        name: event.nft.name,
+        seller: event.seller,
+        buyer: event.buyer,
+        price: Number(event.payment.quantity) / 10 ** 18,
+      };
+    });
   }
+
+  supportsNetwork = (_network: Network): boolean => true;
 }
 
-export const tempActionProvider = () => new TempActionProvider();
+export const openseaActionProvider = ({ apiKey }: { apiKey: string }) =>
+  new OpenseaActionProvider({ apiKey });
